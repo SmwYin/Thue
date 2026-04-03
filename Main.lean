ddd0f9c7baf1fa5dea7377f10f1c6f74d0f6c0b7
SmwYin <xiangmiao.yin@hotmail.com>
2026-04-03 21:27:11 +0100

    Replace with 2026 version

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import Thue2026
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ Thue2026.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `Thue2026` library.
+-- Import modules here that should be built as part of the library.
+import Thue2026.Basic
